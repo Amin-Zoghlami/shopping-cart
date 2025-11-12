c0a42b9d03a5e74a9672757521b0624bfdea331f
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2025-11-11 19:24:44 -0800

    Add basic styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router";
+import styles from "./Nav.module.css";
 
 function Nav() {
   return (
-    <div>
+    <nav className={styles.nav}>
+      <h1>Baseball Junkyard</h1>
       <Link to="/">Home</Link>
       <Link to="/shop">Shop</Link>
       <Link to="/cart">Cart</Link>
-    </div>
+    </nav>
   );
 }
 
