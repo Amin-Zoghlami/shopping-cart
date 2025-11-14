b8ca3ec1e17e27f649b59bd968d74ec7d507b679
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2025-11-13 20:53:51 -0800

    Add groupProduct helper function

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import groupProducts from "../utils/groupProducts";
 
 function Shop() {
   const [products, setProducts] = useState([]);
@@ -8,6 +9,12 @@ function Shop() {
       .then((response) => response.json())
       .then((data) => setProducts(data));
   }, []);
+
+  const gloves = groupProducts(products, "Gloves");
+  const bats = groupProducts(products, "Bats");
+  const helmets = groupProducts(products, "Helmets");
+  const cleats = groupProducts(products, "Cleats");
+  const battingGloves = groupProducts(products, "Batting Gloves");
 
   return <p>Hello, I'm Shop!</p>;
 }
